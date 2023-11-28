dedb8333648bbe4f2b6a59567b6caeeecec985fc
JunHyuk <kkjjhh374@naver.com>
2023-11-28 14:29:51 +0900

    # 10. 로그인 기능 구현 (Context API 사용)

--- src/App.js
+++ src/App.js
@@ -6,30 +6,36 @@ import Footer from './components/layout/Footer';
 import Join from './components/user/Join';
 import Login from './components/user/Login';
 import { Route, Routes } from 'react-router-dom';
+import { AuthContextProvider } from './utils/AuthContext';
 
 const App = () => {
   return (
-    <>
-      <Header />
-      <Routes>
-        <Route
-          path='/'
-          element={<TodoTemplate />}
-        />
-        <Route
-          path='/login'
-          element={<Login />}
-        />
-        <Route
-          path='join'
-          element={<Join />}
-        />
-      </Routes>
-      {/* <TodoTemplate /> */}
-      {/* <Join /> */}
-      {/* <Login /> */}
-      <Footer />
-    </>
+    // 데이터를 전달하고자 하는 자식 컴포너트들을 Provider로 감쌉니다.
+    <AuthContextProvider>
+      <div className='wrapper'>
+        <Header />
+        <div className='content-wrapper'>
+          <Routes>
+            <Route
+              path='/'
+              element={<TodoTemplate />}
+            />
+            <Route
+              path='/login'
+              element={<Login />}
+            />
+            <Route
+              path='join'
+              element={<Join />}
+            />
+          </Routes>
+        </div>
+        {/* <TodoTemplate /> */}
+        {/* <Join /> */}
+        {/* <Login /> */}
+        <Footer />
+      </div>
+    </AuthContextProvider>
   );
 };
 
